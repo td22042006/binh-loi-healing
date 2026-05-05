278ce2b4c234e365546d62114f64bf0c767b7dd8
Phạm Tuấn Đạt <tuandat.21032021@gmail.com>
2026-05-05 21:36:44 +0700

    Support SSL and custom port for database

--- src/core/database.js
+++ src/core/database.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'binhloi_tourism',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    charset: 'utf8mb4'
+    charset: 'utf8mb4',
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : null
 });
 
 module.exports = pool;
