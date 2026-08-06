6357b341d9d83f644f90d3ef2383713bea5a7d99
td22042006 <tuandat@example.com>
2026-08-07 03:04:10 +0700

    Update vercel.json includeFiles for EJS template bundling

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
-module.exports = (req, res) => {
-    try {
-        const app = require('../src/server');
-        return app(req, res);
-    } catch (err) {
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-        return res.status(200).send(`CRITICAL BOOT ERROR ON VERCEL:\n${err.stack || err.message || err}`);
-    }
-};
+const app = require('../src/server');
+
+module.exports = app;
